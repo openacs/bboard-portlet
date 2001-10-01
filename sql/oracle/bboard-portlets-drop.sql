b8c971aded214e7debbb9558ffce8b3ea50d54cf
Yonatan Feldman <yon@milliped.com>
2001-10-01 00:03:53 +0000

    removing package_key

--- sql/oracle/bboard-portlets-drop.sql
+++ sql/oracle/bboard-portlets-drop.sql
@@ -4,15 +4,15 @@
 
 -- Drops bboard datasources for portal portlets
 
--- Copyright (C) 2001 ArsDigita Corporation 
--- @author Michael Bryzek (mbryzek@arsdigita.com)
--- @creation-date 2001-02-26
+-- Copyright (C) 2001 Openforce, Inc. 
+-- @author Arjun Sanyal (arjun@openforce.net)
+-- @creation-date 2001-30-09
 
 -- $Id$
 
 -- This is free software distributed under the terms of the GNU Public
--- License.  Full text of the license is available from the GNU Project:
--- http://www.fsf.org/copyleft/gpl.html
+-- License version 2 or higher.  Full text of the license is available
+-- from the GNU Project: http://www.fsf.org/copyleft/gpl.html
 
 declare  
   ds_id portal_datasources.datasource_id%TYPE;
