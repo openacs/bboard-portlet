d8a950d52dfa28f57033b42734a432fc1feb1684
Yonatan Feldman <yon@milliped.com>
2001-10-01 00:06:17 +0000

    don't do this

--- tcl/bboard-portlets-procs.tcl
+++ tcl/bboard-portlets-procs.tcl
@@ -4,8 +4,8 @@ ad_page_contract {
 
     Procedures to supports bboard portlets
 
-    @author mbryzek@arsdigita.com
-    @creation-date 2001-03-01
+    @author arjun@openforce.net
+    @creation-date September 30 2001
     @$Id$
 }
 
@@ -19,21 +19,25 @@ ad_proc bboard_portlets_mount_application {
     object_id. If no such portal exists, does nothing. This function is
     used as a callback in the acs-subsite group callback system
 
-    @author mbryzek@arsdigita.com
-    @creation-date Fri Feb  9 18:55:22 2001
+    @author arjun@openforce.net
+    @creation-date Sept 2001
 
     @return The package id of the newly mounted package, or the empty
     string if no package was mounted
 
 } {
-    if { [empty_string_p $object_id] } {
-	error "Object ID must be specified"
-    }
-
-    # Find the node ID for the portal associated with this object 
-    set node_id [portal_node_id_for_object $object_id]
-    if { [empty_string_p $node_id] } {
-	return ""
-    }
-    return [subsite::auto_mount_application -node_id $node_id bboard]
+# AKS: don't do this, although it is a neat trick that might be useful
+# in the future of dotlrn
+#
+#
+#    if { [empty_string_p $object_id] } {
+#	 error "Object ID must be specified"
+#    }
+#
+#    # Find the node ID for the portal associated with this object 
+#    set node_id [portal_node_id_for_object $object_id]
+#    if { [empty_string_p $node_id] } {
+#	 return ""
+#    }
+#    return [subsite::auto_mount_application -node_id $node_id bboard]
 }
