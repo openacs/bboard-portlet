4daf61f5d119983eb216f78c85c5bf0950af5383
Yonatan Feldman <yon@milliped.com>
2001-10-01 00:01:43 +0000

    got rid of package_key

--- sql/oracle/bboard-portlets-create.sql
+++ sql/oracle/bboard-portlets-create.sql
@@ -4,22 +4,21 @@
 
 -- Creates bboard datasources for portal portlets
 
--- Copyright (C) 2001 ArsDigita Corporation 
--- @author Phong Nguyen (phong@arsdigita.com)
--- @creation-date 2001-02-26
+-- Copyright (C) 2001 OpenForce, Inc.
+-- @author Arjun Sanyal (arjun@openforce.net)
+-- @creation-date 2001-30-09
 
 -- $Id$
 
 -- This is free software distributed under the terms of the GNU Public
--- License.  Full text of the license is available from the GNU Project:
--- http://www.fsf.org/copyleft/gpl.html
+-- License version 2 or higher.  Full text of the license is available
+-- from the GNU Project: http://www.fsf.org/copyleft/gpl.html
 
 declare
   ds_id portal_datasources.datasource_id%TYPE;
 begin
   ds_id := portal_datasource.new(
     data_type        => 'raw',
-    package_key      => 'bboard',
     mime_type        => 'application/x-ats',
     name             => 'Summarize bboard applications',
     description      => 'Summarizes the bboard topics for all instances of bboard mounted below the current node',
