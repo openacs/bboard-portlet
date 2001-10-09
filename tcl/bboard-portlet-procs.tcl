c4000bca14217aee69dec778b3c3fccee0536bce
Ben Adida <ben@openforce.net>
2001-10-09 03:28:25 +0000

    fixed

--- tcl/bboard-portlet-procs.tcl
+++ tcl/bboard-portlet-procs.tcl
@@ -13,19 +13,24 @@ Licensed under GNU GPL v2
 }
 
 namespace eval bboard_portlet {
-
+    
     ad_proc -private my_name {
     } {
-    return "bboard-portlet"
+	return "bboard-portlet"
     }
-
+    
+    ad_proc -public get_pretty_name {
+    } {
+	return "Discussion Forums"
+    }
+    
     ad_proc -public add_self_to_page { 
 	page_id 
 	instance_id 
     } {
 	Adds a bboard PE to the given page with the instance key being
 	opaque data in the portal configuration.
-    
+	
 	@return element_id The new element's id
 	@param page_id The page to add self to
 	@param instance_id The bboard instace to show
@@ -38,25 +43,25 @@ namespace eval bboard_portlet {
 	# The default param "instance_id" must be configured
 	set key "instance_id"
 	set value [portal::get_element_param $element_id $key]
-
+	
 	portal::set_element_param $element_id $key $instance_id
-
+	
 	return $element_id
     }
-
-    ad_proc -public show { 
-	 cf 
-    } {
-	 Display the PE
     
-	 @return HTML string
-	 @param cf A config array
-	 @author arjun@openforce.net
-	 @creation-date Sept 2001
+    ad_proc -public show { 
+	cf 
     } {
-
+	Display the PE
+	
+	@return HTML string
+	@param cf A config array
+	@author arjun@openforce.net
+	@creation-date Sept 2001
+    } {
+	
 	array set config $cf	
-
+	
 	set query  "
         select 	message_id, 
 	title, 
@@ -69,15 +74,15 @@ namespace eval bboard_portlet {
 	and person_id = sender
 	and reply_to is null
 	order by sent_date desc"
-
+	
 	set data ""
 	set rowcount 0
-
+	
 	db_foreach select_messages $query {
 	    append data "<tr><td>$title</td><td>$full_name</td><td>$num_replies</td><td>$last_updated</td>"
 	    incr rowcount
 	} 
-
+	
 	set template "
 	<table celpadding=\"0\" cellspacing=\"0\" border=\"0\">
 	<tr bgcolor=\"#ECECEC\">
@@ -88,55 +93,47 @@ namespace eval bboard_portlet {
 	</tr>
 	$data
 	</table>"
-
+	
 	ns_log notice "AKS31 got here $rowcount"
-
+	
 	if {!$rowcount} {
 	    set template "<i>No messages</i>"
 	}
+	
+	# Ben addition
+	append template "<p><a href=bboard/>more...</a>"
 
 	set code [template::adp_compile -string $template]
-
+	
 	set output [template::adp_eval code]
 	ns_log notice "AKS32 got here $output"
 	
 	return $output
-
-    }
-
-    ad_proc -public remove_self_from_page { 
-	  portal_id 
-	  instance_id 
-    } {
-	  Removes a bboard PE from the given page 
+	
+    }   
     
-	  @param page_id The page to remove self from
-	  @param instance_id
-	  @author arjun@openforce.net
-	  @creation-date Sept 2001
+    ad_proc -public remove_self_from_page { 
+	portal_id 
+	instance_id 
     } {
-	 # Find out the element_id that corresponds to this instance_id
-	 if { [db0or1row get_element_id "
-	 select pem.element_id as element_id
-	 from portal_element_parameters pep, portal_element_map pem
-	 where pem.portal_id = $portal_id and
-	 pep.element_id = pem.element_id and
-	 pep.key = 'instance_id' and
-	 pep.value = $instance_id"]  } {
-	     
-	     # delete the params
-	     # delete the element from the map
-	     ns_log Notice "foo"
+	Removes a bboard PE from the given page 
+	
+	@param page_id The page to remove self from
+	@param instance_id
+	@author arjun@openforce.net
+	@creation-date Sept 2001
+    } {
+	# get the element IDs (could be more than one!)
+	set element_ids [portal::get_element_ids_by_ds $portal_id [my_name]]
 
-	 } else {
-	     ad_return_complaint 1 "bboard_portlet::remove_self_from_page: Invalid portal_id and/or instance_id given."
-	     ad_script_abort
-	 }
-
-	 # this call removes the PEs params too
-	 set element_id [portal::remove_element {$portal_id $element_id}]
-     }
- }
-
+	# remove all elements
+	db_transaction {
+	    foreach element_id $element_ids {
+		portal::remove_element $element_id
+	    }
+	}
+    }
+    
+}
  
 
