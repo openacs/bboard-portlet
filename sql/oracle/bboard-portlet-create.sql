59693cc7575939c4d931c48d06f67693c6c17106
Yonatan Feldman <yon@milliped.com>
2001-11-17 22:08:04 +0000

    sc

--- sql/oracle/bboard-portlet-create.sql
+++ sql/oracle/bboard-portlet-create.sql
@@ -78,3 +78,112 @@ end;
 /
 show errors
 
+
+declare
+	foo integer;
+begin
+	-- create the implementation
+	foo := acs_sc_impl.new (
+		'portal_datasource',
+		'bboard_portlet',
+		'bboard_portlet'
+	);
+
+end;
+/
+show errors
+
+declare
+	foo integer;
+begin
+
+	-- add all the hooks
+	foo := acs_sc_impl.new_alias (
+	       'portal_datasource',
+	       'bboard_portlet',
+	       'MyName',
+	       'bboard_portlet::my_name',
+	       'TCL'
+	);
+
+	foo := acs_sc_impl.new_alias (
+	       'portal_datasource',
+	       'bboard_portlet',
+	       'GetPrettyName',
+	       'bboard_portlet::get_pretty_name',
+	       'TCL'
+	);
+
+	foo := acs_sc_impl.new_alias (
+	       'portal_datasource',
+	       'bboard_portlet',
+	       'Link',
+	       'bboard_portlet::link',
+	       'TCL'
+	);
+
+	foo := acs_sc_impl.new_alias (
+	       'portal_datasource',
+	       'bboard_portlet',
+	       'AddSelfToPage',
+	       'bboard_portlet::add_self_to_page',
+	       'TCL'
+	);
+
+	foo := acs_sc_impl.new_alias (
+	       'portal_datasource',
+	       'bboard_portlet',
+	       'Show',
+	       'bboard_portlet::show',
+	       'TCL'
+	);
+
+	foo := acs_sc_impl.new_alias (
+	       'portal_datasource',
+	       'bboard_portlet',
+	       'Edit',
+	       'bboard_portlet::edit',
+	       'TCL'
+	);
+
+	foo := acs_sc_impl.new_alias (
+	       'portal_datasource',
+	       'bboard_portlet',
+	       'RemoveSelfFromPage',
+	       'bboard_portlet::remove_self_from_page',
+	       'TCL'
+	);
+
+	foo := acs_sc_impl.new_alias (
+	       'portal_datasource',
+	       'bboard_portlet',
+	       'MakeSelfAvailable',
+	       'bboard_portlet::make_self_available',
+	       'TCL'
+	);
+
+	foo := acs_sc_impl.new_alias (
+	       'portal_datasource',
+	       'bboard_portlet',
+	       'MakeSelfUnavailable',
+	       'bboard_portlet::make_self_unavailable',
+	       'TCL'
+	);
+
+end;
+/
+show errors
+
+declare
+	foo integer;
+begin
+
+	-- Add the binding
+	acs_sc_binding.new (
+	    contract_name => 'portal_datasource',
+	    impl_name => 'bboard_portlet'
+	);
+end;
+/
+show errors
+
