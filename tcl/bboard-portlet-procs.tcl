4a634978f4f8bba8b11b52580e3ff5bee15e8d1a
Ben Adida <ben@openforce.net>
2001-12-01 23:18:49 +0000

    updated the whole architecture to do dotlrn_user_rel, and portal-id

--- tcl/bboard-portlet-procs.tcl
+++ tcl/bboard-portlet-procs.tcl
@@ -30,7 +30,7 @@ namespace eval bboard_portlet {
     }
     
     ad_proc -public add_self_to_page { 
-	page_id 
+	portal_id 
 	instance_id 
 	args
     } {
@@ -38,7 +38,7 @@ namespace eval bboard_portlet {
 	opaque data in the portal configuration.
 	
 	@return element_id The new element's id
-	@param page_id The page to add self to
+	@param portal_id The page to add self to
 	@param instance_id The bboard instace to show
 	@param args an arg string not used
 	@author arjun@openforce.net
@@ -46,11 +46,11 @@ namespace eval bboard_portlet {
     } {
 	# Add some smarts to only add one portlet for now when it's added multiple times (ben)
 	# Find out if bboard already exists
-	set element_id_list [portal::get_element_ids_by_ds $page_id [my_name]]
+	set element_id_list [portal::get_element_ids_by_ds $portal_id [my_name]]
 
 	if {[llength $element_id_list] == 0} {
 	    # Tell portal to add this element to the page
-	    set element_id [portal::add_element $page_id [my_name]]
+	    set element_id [portal::add_element $portal_id [my_name]]
 	    # There is already a value for the param which must be overwritten
 	    portal::set_element_param $element_id instance_id $instance_id
 	    set package_id_list [list]
@@ -170,7 +170,7 @@ namespace eval bboard_portlet {
     } {
 	Removes a bboard PE from the given page 
 	
-	@param page_id The page to remove self from
+	@param portal_id The page to remove self from
 	@param instance_id
 	@author arjun@openforce.net
 	@creation-date Sept 2001
@@ -193,29 +193,29 @@ namespace eval bboard_portlet {
     }
 
     ad_proc -public make_self_available { 
-	page_id 
+	portal_id 
     } {
 	Wrapper for the portal:: proc
 	
-	@param page_id
+	@param portal_id
 	@author arjun@openforce.net
 	@creation-date Nov 2001
     } {
 	portal::make_datasource_available \
-		$page_id [portal::get_datasource_id [my_name]]
+		$portal_id [portal::get_datasource_id [my_name]]
     }
 
     ad_proc -public make_self_unavailable { 
-	page_id 
+	portal_id 
     } {
 	Wrapper for the portal:: proc
 	
-	@param page_id
+	@param portal_id
 	@author arjun@openforce.net
 	@creation-date Nov 2001
     } {
 	portal::make_datasource_unavailable \
-		$page_id [portal::get_datasource_id [my_name]]
+		$portal_id [portal::get_datasource_id [my_name]]
     }
 }
  
